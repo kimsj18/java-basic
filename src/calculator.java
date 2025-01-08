a1eb459b4723a4d4c93fd1af9ba67c8866d46b38
kimsj18 <kimsj0118@gmail.com>
2025-01-08 16:33:26 +0900

    "first commit"

---
+++ src/calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class calculator {
+        //1 + 1 =?
+    public static void main(String[] args) {
+        //1. 데이터 자유 입력\
+        Scanner in = new Scanner(System.in);
+        System.out.println("first number input: ");
+            int a = in.nextInt();  //1
+            System.out.println("emd number input: ");
+            int b = in.nextInt();  //100
+            int sum = 0;
+        //2. 데이터 처리
+
+        //3. 데이터 결과 저장
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("i = " + i);
+            if(i % 2 == 0){
+            sum = sum + i;}
+            System.out.println(+ sum);
+
+            //for문을 이용하여 1-100까지의 합을 구하는 결과 출력
+        }
+        //4. 결과 출력
+
+        System.out.println("결과값 => " + a + "+" + b + " = " + sum );
+
+    }
+}
---
+++ src/ex1.java
@@ -0,0 +1,9 @@
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("한 줄에 여러 문장을 출력합니다..");
+        System.out.println("세 번째 프로그램입니다. 줄을 바꾸어 출력합니다. 여기까지 출력하고");
+        System.out.println("줄을 바꾸어 출려합니다");
+    }
+
+}
